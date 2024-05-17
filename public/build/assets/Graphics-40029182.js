29197b627460db777f26b88a4aa8731504be892c
GuillexD <enrique2003junior@gmail.com>
2024-05-17 12:18:16 -0500

    colors

--- public/build/assets/Graphics-40029182.js
+++ public/build/assets/Graphics-40029182.js
@@ -1,4 +1,4 @@
-var so=Object.defineProperty;var no=(i,t,e)=>t in i?so(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var M=(i,t,e)=>(no(i,typeof t!="symbol"?t+"":t,e),e);import{r as et,R as dn,j as L,K as oo,L as ro,M as ni,F as ao,I as lo,C as co,c as ho}from"./Footer-a0a9b1e3.js";/*!
+var so=Object.defineProperty;var no=(i,t,e)=>t in i?so(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var M=(i,t,e)=>(no(i,typeof t!="symbol"?t+"":t,e),e);import{r as et,R as dn,j as L,K as oo,L as ro,M as ni,F as ao,I as lo,C as co,c as ho}from"./Footer-3e7d975a.js";/*!
  * @kurkle/color v0.3.2
  * https://github.com/kurkle/color#readme
  * (c) 2023 Jukka Kurkela
